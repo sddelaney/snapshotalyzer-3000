c37fafddaf39a48560afe5a7935885aa79a84484
Steven Delaney <steven@Stevens-MBP.fios-router.home>
2018-07-14 11:38:39 -0400

    Dont start if already in progress - snapshot

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -17,6 +17,10 @@ def filter_instances(project):
 
         return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -91,6 +95,9 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print(" Skipping {0}, already in progress".format(v.id))
+                continue
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Snapshotalyzer 3000")
 
